d39648f4f920310e4596d0d2e267bcf94c3ea488
sgupta1025 <samir@vcf2.cs.uga.edu>
2019-04-30 17:30:48 -0400

    Started export command

--- shell.cpp
+++ shell.cpp
@@ -10,6 +10,8 @@ int main(int argc, char* argv[], char* envp[])
 	string command;
 	string dir= get_current_dir_name();
 	string homeDir = getenv("HOME");
+	char* strArray[11];
+	char* token;
 	do
 	{
 
@@ -19,6 +21,25 @@ int main(int argc, char* argv[], char* envp[])
 		}
 		cout << "1730sh:" << dir << "$ ";
 		cin >> command;
+		token = strtok(&command[0], " ");
+		int num =0;
+		while (token != NULL)
+		{
+			//cout << token << endl;
+			strArray[num++] = token;
+			token = strtok(NULL, " ");
+		}
+		for(int i =0; i < 10; i++)
+		{
+			if(i >= num)
+			{
+				strArray[i] = NULL;
+			}
+		}
+		if(strcmp(strArray[0], "export") == 0)
+		{
+			cout << "do export" << endl;
+		}
 		cout << command << endl;
 	}
 	while(command.compare("exit") !=0);
